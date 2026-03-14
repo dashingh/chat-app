292e1da7a762edc2509269bc28aff460ab6204b0
harshal panchal <harshalp@gorentals.com>
2026-03-14 16:20:50 +0530

    inital config

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -2,7 +2,11 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI as string);
+    const MongoDB_URI = process.env.MONGODB_URI;
+    if (!MongoDB_URI) {
+      throw new Error("MONGODB_URI is not defined");
+    }
+    await mongoose.connect(MongoDB_URI as string);
     console.log("Connected to MongoDB");
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
