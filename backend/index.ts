12bd45df316ebbe37f00c156f9bb65d5c5f65098
harshal panchal <harshalp@gorentals.com>
2026-03-14 16:19:09 +0530

    inital config

--- backend/index.ts
+++ backend/index.ts
@@ -3,8 +3,13 @@ import { connectDB } from "./src/config/database";
 
 const PORT = process.env.PORT || 3000;
 
-connectDB().then(() => {
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+    process.exit(1);
   });
-});
